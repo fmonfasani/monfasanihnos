970f89ac07d0a94193629b8a0ed513d32920f24f
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2025-08-28 23:56:15 -0300

    Fix static asset path for Vercel

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
-  } else {
+  } else if (!process.env.VERCEL) {
     serveStatic(app);
   }
 
